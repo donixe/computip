fde9fe58b03b8355b5c8999805a23bdffbc67e35
donixe <doni0604@rocketmail.com>
2017-04-12 17:28:44 +0700

    Create computip-common.js

---
+++ computip-common.js
@@ -0,0 +1,13 @@
+function viewport() {
+    var e = window, a = 'inner';
+    if (!('innerWidth' in window )) {
+        a = 'client';
+        e = document.documentElement || document.body;
+    }
+    return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+}
+
+function screen_width() {
+	var screen = viewport();
+	return screen['width'];
+}
